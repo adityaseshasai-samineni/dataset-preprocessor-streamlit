6fba5405ad3c120bc06b581eaae4bb860d966b90
Aditya Sesha Sai Samineni <89957932+adityaseshasai-samineni@users.noreply.github.com>
2024-02-25 19:10:55 +0530

    Update app.py
    modified feature scaling page and description and resolved few issues

--- app.py
+++ app.py
@@ -35,14 +35,37 @@ def data_description_page():
     if st.button("Describe a column"):
         st.session_state.page = "column_form"
     if st.button("Dataset's Properties"):
-        st.write(st.session_state.df.describe(include='all'))
+        st.table(st.session_state.df.describe(include='all'))
         buf = StringIO()
         st.session_state.df.info(buf=buf)
-        st.text(buf.getvalue())
+        df_info = buf.getvalue()
+
+        info_data = []
+        sno = 1
+        parse_started = False
+        for line in df_info.split('\n'):
+            if line.startswith('dtypes'):
+                parse_started = False
+            if parse_started:
+                if line.strip():
+                    columns = line.split()
+                    col_name = columns[1]
+                    non_null_count = columns[2]
+                    dtype = columns[4]
+                    info_data.append([sno, col_name, non_null_count, dtype])
+                    sno += 1
+            elif line.startswith('---'):
+                parse_started = True
+
+        # Creating a DataFrame from the parsed info data
+        info_df = pd.DataFrame(info_data, columns=['sno', 'Column', 'Non-Null Count', 'Dtype'])
+
+        # Displaying the DataFrame as a table
+        st.table(info_df)
     if st.button("show DataSet"):
-        st.write(st.session_state.df)
-    if st.button("Back"):
-        st.session_state.page = "home"
+        st.table(st.session_state.df)
+    # if st.button("Back"):
+    #     st.session_state.page = "home"
 
 def column_describe_page():
     with st.sidebar:
@@ -60,18 +83,15 @@ def column_describe_page():
 
     with st.form(key='column_form'):
             st.header('Columns')
-            st.write(st.session_state.df.columns.to_list())
-            col_name = st.text_input("Enter Column Name")
-            submit_button = st.form_submit_button("Enter")
+            columns = st.session_state.df.columns.to_list()
+            col_name = st.session_state.selected_column = st.selectbox("Select a column to normalize", columns)
+            submit_button = st.form_submit_button("Submit")
             if submit_button:
                 if col_name in st.session_state.df.columns.to_list():
-                    st.write(st.session_state.df[col_name].describe())
+                    st.table(st.session_state.df[col_name].describe())
                 else:
                     st.write(f"The column '{col_name}' does not exist in the DataFrame.")
-            else:
-                st.write("The 'Describe a column' button hasn't been clicked.")
-    if st.button("Back"):
-        st.session_state.page = "data_description"
+
 
 def input():
     st.title("Dataset Preprocessor")
@@ -110,9 +130,7 @@ def null_val_handle_page():
     if st.button("Fill NULL values"):
         st.session_state.page = "fill_null_val"
     if st.button("show DataSet"):
-        st.write(st.session_state.df)
-    if st.button("Back"):
-        st.session_state.page = "home"
+        st.table(st.session_state.df)
 
 def remove_col_page():
     with st.sidebar:
@@ -129,20 +147,16 @@ def remove_col_page():
             st.session_state.page = "download"
 
     with st.form(key='remove_col'):
-            st.header('Remove Columns')
-            st.write(st.session_state.df.columns.to_list())
-            col_name = st.text_input("Enter Column Name")
-            submit_button = st.form_submit_button("Enter")
+            columns = st.session_state.df.columns.to_list()
+            col_name = st.session_state.selected_column = st.selectbox("Select a column to remove it.", columns)
+            submit_button = st.form_submit_button("Submit")
             if submit_button:
                 if col_name in st.session_state.df.columns.to_list():
                     st.session_state.df = st.session_state.df.drop([col_name], axis=1)
                     st.write(f"The column '{col_name}' Dropped")
                 else:
                     st.write(f"The column '{col_name}' does not exist in the DataFrame.")
-            else:
-                st.write("The 'Describe a column' button hasn't been clicked.")
-    if st.button("Back"):
-        st.session_state.page = "Hand_null_val"
+
 
 def fill_null_val():
     with st.sidebar:
@@ -160,13 +174,14 @@ def fill_null_val():
 
     with st.form(key='fill_null_val'):
             st.header('Fill NULL values')
-            st.write(st.session_state.df.columns.to_list())
-            col_name = st.text_input("Enter Column Name")
-            submit_button = st.form_submit_button("Enter")
+            columns = st.session_state.df.columns.to_list()
+            col_name = st.session_state.selected_column = st.selectbox("Select a column to FILL", columns)
+            submit_button = st.form_submit_button("Submit")
             if submit_button:
                 if col_name in st.session_state.df.columns.to_list():
                     if st.form_submit_button("Mean"):
                         st.session_state.df = st.session_state.df[col_name].fillna(st.session_state.df[col_name].mean(), inplace=True)
+                        st.write(f"The column '{col_name}' Filled")
                     if st.form_submit_button("Median"):
                         st.session_state.df = st.session_state.df[col_name].fillna(st.session_state.df[col_name].median(), inplace=True)
                     if st.form_submit_button('Mode'):
@@ -174,10 +189,6 @@ def fill_null_val():
                     st.write(f"The column '{col_name}' Filled")
                 else:
                     st.write(f"The column '{col_name}' does not exist in the DataFrame.")
-            else:
-                st.write("The 'Describe a column' button hasn't been clicked.")
-    if st.button("Back"):
-        st.session_state.page = "Hand_null_val"
 
 def encode_data():
     with st.sidebar:
@@ -193,24 +204,22 @@ def encode_data():
         if st.button("Download the dataset"):
             st.session_state.page = "download"
 
-    if st.button("Show Categorical Columns"):
-        categorical_columns = st.session_state.df.select_dtypes(include=['object'])
-        unique_counts = {col: st.session_state.df[col].nunique() for col in categorical_columns}
-        unique_counts_df = pd.DataFrame(list(unique_counts.items()), columns=['Column', 'Unique Count'])
-        st.table(unique_counts_df)
+    st.header("Categorical Columns")
+    categorical_columns = st.session_state.df.select_dtypes(include=['object'])
+    unique_counts = {col: st.session_state.df[col].nunique() for col in categorical_columns}
+    unique_counts_df = pd.DataFrame(list(unique_counts.items()), columns=['Column', 'Unique Count'])
+    st.table(unique_counts_df)
     
     categorical_columns = st.session_state.df.select_dtypes(include=['object']).columns.tolist()
     selected_column = st.selectbox("Select a column to encode", categorical_columns)
 
-    if st.button("Encode Categorical Columns"):
+    if st.button("Submit"):
         st.session_state.df = pd.get_dummies(st.session_state.df, columns=[selected_column])
-        st.write(st.session_state.df)
+        st.table(st.session_state.df)
     
     if st.button("show DataSet"):
-        st.write(st.session_state.df)
+        st.table(st.session_state.df)
 
-    if st.button("Back"):
-        st.session_state.page = "home"
 
 def feature_scale():
     with st.sidebar:
@@ -233,47 +242,61 @@ def feature_scale():
     if "standardize_scaler" not in st.session_state:
         st.session_state.standardize_scaler = False
 
-    if st.button("Normalization (MinMax Scaler)"):
-        st.session_state.normalize_scaler = True
-
-    if st.session_state.normalize_scaler:
+    button_placeholder_norm = st.empty()
+    button_placeholder_stan = st.empty()
+    button_placeholder_show = st.empty()
+# Create a button inside the placeholder
+    button_clicked1 = button_placeholder_norm.button("Normalization (MinMax Scaler)")
+    button_clicked2 = button_placeholder_stan.button("Standardization (Standard Scaler)")
+    button_clicked3 = button_placeholder_show.button("Show DataSet")
+# If the button is clicked, clear the placeholder to hide the button
+    if button_clicked1:
+        button_placeholder_norm.empty()
+        button_placeholder_stan.empty()
+        button_placeholder_show.empty()
         if st.button("Normalize whole dataset"):
             scaler = MinMaxScaler()
             st.session_state.df[numeric_columns] = scaler.fit_transform(st.session_state.df[numeric_columns])
-            st.write(st.session_state.df)
+            st.table(st.session_state.df)
 
-        
+        st.write("OR")
         st.session_state.selected_column = st.selectbox("Select a column to normalize", numeric_columns)
 
         if st.button("Normalize a column"):
             scaler = MinMaxScaler()
             st.session_state.df[st.session_state.selected_column] = scaler.fit_transform(st.session_state.df[[st.session_state.selected_column]])
-            st.write(st.session_state.df)
+            st.table(st.session_state.df)
+        if st.button("back"):
+            button_clicked1 = button_placeholder_norm.button("Normalization (MinMax Scaler)")
+            button_clicked2 = button_placeholder_stan.button("Standardization (Standard Scaler)")
 
-    if st.button("Standardization (Standard Scaler)"):
-        st.session_state.normalize_scaler = False
-        st.session_state.standardize_scaler = True
-    
-    if st.session_state.standardize_scaler:
+    if button_clicked2:
+        button_placeholder_norm.empty()
+        button_placeholder_stan.empty()
+        button_placeholder_show.empty()
         if st.button("Standardize whole dataset"):
             scaler = StandardScaler()
             st.session_state.df[numeric_columns] = scaler.fit_transform(st.session_state.df[numeric_columns])
-            st.write(st.session_state.df)
-
+            st.table(st.session_state.df)
+        st.write("OR")
         st.session_state.selected_column = st.selectbox("Select a column to standardize", numeric_columns)
 
-        if st.button("Standardize a column"):
+        if st.button("Submit"):
             scaler = StandardScaler()
             st.session_state.df[st.session_state.selected_column] = scaler.fit_transform(st.session_state.df[[st.session_state.selected_column]])
-            st.write(st.session_state.df)
-
-    if st.button("show DataSet"):
-        st.write(st.session_state.df)
+            st.table(st.session_state.df)
+        if st.button("back"):
+            button_clicked1 = button_placeholder_norm.button("Normalization (MinMax Scaler)")
+            button_clicked2 = button_placeholder_stan.button("Standardization (Standard Scaler)")
     
-    if st.button("Back"):
-        st.session_state.normalize_scaler = False
-        st.session_state.page = "home"
-
+    if button_clicked3:
+        button_placeholder_norm.empty()
+        button_placeholder_stan.empty()
+        button_placeholder_show.empty()
+        st.table(st.session_state.df)
+        if st.button("back"):
+            button_clicked1 = button_placeholder_norm.button("Normalization (MinMax Scaler)")
+            button_clicked2 = button_placeholder_stan.button("Standardization (Standard Scaler)")
 
 def download():
     with st.sidebar:
@@ -292,13 +315,13 @@ def download():
     st.header("Download the preprocessed dataset")
     file_name = st.text_input("Enter the file name", "preprocessed_data.csv")
 
-    if st.button("Save"):
+    if st.button("Download"):
         st.session_state.df.to_csv(file_name, index=False)
         st.markdown(f"File saved as {file_name} in the current directory: {os.getcwd()}")
-    if st.button("Back"):
-        st.session_state.page = "home"
+
 
 def main():
+    st.set_page_config(page_title="Dataset Preprocessor",layout="wide")
     if 'page' not in st.session_state:
         st.session_state.page = "input"
     # Display the selected page
